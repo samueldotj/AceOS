43f8cb777ec3abd3ef4bce9727c58f8a9df83c23
nmdilipsimha <nmdilipsimha@78f6c469-4e3a-0410-bca2-dfd793a85426>
2008-03-20 09:05:09 +0000

    Created heap and slab allocator files to start with.
    git-svn-id: svn://svn.code.sf.net/p/aceos/code@122 78f6c469-4e3a-0410-bca2-dfd793a85426

---
+++ src/include/heap/heap.h
@@ -0,0 +1,15 @@
+/*!
+  \file		heap.h
+  \author	DilipSimha N M
+  \version 	3.0
+  \date	
+  			Created:
+  			Last modified: 
+  \brief	
+*/
+
+
+#ifndef _HEAP_H_
+#define _HEAP_H_
+
+#endif
---
+++ src/include/heap/slab_allocator.h
@@ -0,0 +1,15 @@
+/*!
+  \file		slab_allocator.h
+  \author	DilipSimha N M
+  \version 	3.0
+  \date	
+  			Created:
+  			Last modified: 
+  \brief	
+*/
+
+
+#ifndef _SLAB_ALLOCATOR_H_
+#define _SLAB_ALLOCATOR_H_
+
+#endif

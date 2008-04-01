51514163beac9d1e784149eb0c16b74d0ebc2cba
nmdilipsimha <nmdilipsimha@78f6c469-4e3a-0410-bca2-dfd793a85426>
2008-04-01 19:30:37 +0000

    Made some slab_allocator modifications: Formatting, malloc() functionality based on newly created data structures.
    git-svn-id: svn://svn.code.sf.net/p/aceos/code@139 78f6c469-4e3a-0410-bca2-dfd793a85426

--- src/include/heap/slab_allocator.h
+++ src/include/heap/slab_allocator.h
@@ -4,8 +4,8 @@
   \version 	3.0
   \date	
   			Created:	Fri Mar 21, 2008  09:04PM
-  			Last modified: Mon Mar 24, 2008  12:37PM
-  \brief	This file contains structures to maintain slab_allocator
+  			Last modified: Wed Apr 02, 2008  12:26AM
+  \brief	This file contains structures and macros to maintain slab_allocator
 */
 
 
@@ -17,53 +17,78 @@
 #include <ds/list.h>
 #include <kernel/spinlock.h>
 
-/*define this macro to enable stat*/
+/* define this macro to enable statistics */
 #define SLAB_STAT_ENABLED
-/*define this macro to debug the slab alloctor*/
+/* define this macro to debug the slab alloctor */
 #define SLAB_DEBUG_ENABLED
 
-struct slab_allocator_metadata
+typedef struct slab_allocator_metadata
 {
 	UINT32	vm_page_size;
 	void * (*VirtualAlloc)(int size);
-	void * (*VirtualFree)(void * va, int size),
+	void * (*VirtualFree)(void * va, int size);
 	void * (*VirtualProtect)(void * va, int size, int protection);
-}SLAB_ALLOCATOR_METADATA, * SLAB_ALLOCATOR_METADATA_PTR;
+} SLAB_ALLOCATOR_METADATA, * SLAB_ALLOCATOR_METADATA_PTR;
 
 
 #ifdef SLAB_DEBUG_ENABLED
 	UINT32	slab_debug_options = 0;
 #endif
+
+
 typedef struct slab {
 	UINT16		used_buffer_count;
-	LIST		in_use_list;/*It is ordered list of slabs which has at least one free buffer and at least one in use buffer. It is used during buffer allocation to find a free buffer in the slab.*/
-	AVL_TREE	in_use_tree;/*It is tree of slabs which has at least one in use buffer. It is used during buffer free to find the slab to which the buffer is belongs to.*/
-	LIST		free_list;/*It is list of completely free slabs(all the buffers are free). It is used to release the memory pages back to the vm.*/
-	UINT32		buffer_usage_bitmap[0];/*if a bit is set the corresponding buffer is used else free*/
+
+	LIST		partially_free_list;
+	/* It is ordered list of slabs which has at least one free buffer 
+	 * and atleast one in use buffer.
+	 * It is used during buffer allocation to find a free buffer in the slab.
+	 */
+
+	AVL_TREE	in_use_tree;
+	/* It is tree of slabs which has at least one in use buffer. 
+	 * It is used during buffer free, to find the slab to which the buffer belongs to.
+	 */
+
+	LIST		completely_free_list;
+	/* It is a list of completely free slabs (all the buffers within the slab are free). 
+	 * It is used to release the memory pages back to the vm.
+	 */
+
+	UINT32		buffer_usage_bitmap[0];
+	/* If a bit is set, the corresponding buffer is used; else free */
 } SLAB, *SLAB_PTR;
 
 typedef struct cache {
 	SPIN_LOCK	slock;
 
-	int			size;	/* size of buffers available from this cache */
-	int			(*constructor)(void * data);/* initializes a given buffer */
-	int			(*destructor)(void * data);/* points to a function whichmakes the buffer reusable */
+	int		size; /* size of buffers available from this cache */
+	int		(*constructor)(void * data); /* initializes a given buffer */
+	int		(*destructor)(void * data); /* points to a function which makes the buffer reusable */
 
-	int			min_slabs;/* Minimum no of slabs to be present always */
-	int			max_slabs;/* Maximum no of slabs allowed */
+	int		min_slabs; /* Minimum no of slabs to be present always */
+	int		max_slabs; /* Maximum no of slabs allowed */
 
-	int			free_slabs_threshold; /* Threshold to start VM operation */
-	int			free_slabs_count; /* count of free slabs in the free slab list */
+	int		free_slabs_threshold; /* Threshold to start VM operation */
+	int		free_slabs_count; /* count of free slabs in the completely free slab list */
 
-	SLAB_PTR	in_use_slab_tree_root;/* A tree to store in use slabs which is used while freeing to find the slab for the given address*/
-	SLAB_PTR	completely_free_slab_list;/* List of completely free slabs which can be freed to VM or used again*/
+	SLAB_PTR	in_use_slab_tree_root;
+	/* A tree to store in use slabs, which is used while freeing to
+	 * find a slab for the given address.
+	 */
+	SLAB_PTR	completely_free_slab_list;
+	/* List of completely free slabs which can be freed to VM or used again */
+
+	SLAB_PTR partially_free_slab_list;
+	/* List of partially free slabs which have some buffers free */
 	
 #ifdef SLAB_STAT_ENABLED
-	UINT32		alloc_calls;/*total allocation calls from the user*/
-	UINT32		free_calls;/*total free calls from the user*/
-	UINT32		vm_alloc_calls;/*total calls to vm to get memory pages*/
-	UINT32		vm_free_calls;/*total calls to vm to free memory pages*/
-	UINT32		free_buffer_end;/*number of times alloc failed to find a free buffer in slab list*/
+	UINT32		alloc_calls; /* total allocation calls from the user */
+	UINT32		free_calls; /* total free calls from the user */
+	UINT32		vm_alloc_calls; /* total calls to vm to get memory pages */
+	UINT32		vm_free_calls; /* total calls to vm to free memory pages */
+	UINT32		free_buffer_end;
+	/* Number of times alloc failed to find a free buffer in the slab list */
 	
 	UINT32		max_slabs_used;
 	UINT32		average_slab_usage;
@@ -71,19 +96,23 @@ typedef struct cache {
 
 } CACHE, *CACHE_PTR;
 
-/*initializes the cache subsystem*/
+/*initializes the Slab Allocator subsystem*/
 void InitSlabAllocator(UINT32 page_size, void * (*v_alloc)(int size), 
 		void * (*v_free)(void * va, int size),
-		void * (*v_protect)(void * va, int size, int protection),
+		void * (*v_protect)(void * va, int size, int protection)
        );
 
 /*initializes a cache*/
-int CacheCreate(CACHE_PTR new_cache, UINT32 size, int min_slabs,  int max_slabs, int free_slabs_threshold, int (*constructor)(void *), int (*destructor)(void) );
+int CacheCreate(CACHE_PTR new_cache, UINT32 size, int free_slabs_threshold,
+		int min_slabs, int max_slabs,
+		int (*constructor)(void *), int (*destructor)(void *));
 
 /*allocates memory from the specified cache*/
 void * CacheAlloc(CACHE_PTR);
+
 /*frees memory to the specified cache*/
 void CacheFree(CACHE_PTR, void *);
+
 /*destroys the specified cache*/
 void CacheDestroy(CACHE_PTR);
 
--- src/lib/heap/slab_allocator.c
+++ src/lib/heap/slab_allocator.c
@@ -25,10 +25,20 @@ static SLAB_ALLOCATOR_METADATA slab_alloactor_metadata;
 #endif
 
 #ifdef	SLAB_DEBUG_ENABLED
-	#define BUFFER_SIZE(size)		(size+SLAB_DEBUG_PAD_SIZE)
+	#define BUFFER_SIZE(size)		((size)+SLAB_DEBUG_PAD_SIZE)
 #else
 	#define BUFFER_SIZE(size)		(size)
 #endif
+
+
+/* Function declarations */
+
+static VADDR GetFreeBufferFromSlab(SLAB_PTR free_slab);
+static int AddSlabToCache (CACHE_PTR cache_entry);
+
+
+
+/* Function definitions */
 
 void InitSlabAllocator(UINT32 page_size, void * (*v_alloc)(int size), 
 	void * (*v_free)(void * va, int size),
@@ -58,18 +68,18 @@ void InitSlabAllocator(UINT32 page_size, void * (*v_alloc)(int size),
 */
 int CacheCreate (CACHE_PTR new_cache, UINT32 size,
 		int free_slabs_threshold, int min_slabs, int max_slabs,
-		int (*constructor)(CACHE_PTR, SLAB_PTR), 
-		int (*destructor)(CACHE_PTR, SLAB_PTR))
+		int (*constructor)(void *buffer), 
+		int (*destructor)(void *buffer))
 {
 	new_cache->size = size;
 	new_cache->constructor = constructor;
 	new_cache->destructor = destructor;
-	new_cache->free_slab_list = NULL;
+	new_cache->completely_free_slab_list = NULL;
+	new_cache->partially_free_slab_list = NULL;
 	new_cache->min_slabs = min_slabs; 
 	new_cache->max_slabs = max_slabs;
 	new_cache->free_slabs_count = 0;
-	new_cache->allocated_slab_tree = NULL;
-	new_cache->free_buffer_list = NULL;
+	new_cache->in_use_slab_tree_root = NULL;
 	new_cache->free_slabs_threshold = free_slabs_threshold;
 	return 0;
 }
@@ -77,14 +87,14 @@ int CacheCreate (CACHE_PTR new_cache, UINT32 size,
 void* malloc (UINT32 size, CACHE_PTR *cache_entry)
 {
 	SLAB_PTR free_slab;
-	FREE_BUFFER_LIST_PTR free_buffer;
 	UINT32 free_slabs_count;
+	VADDR ret_va;
 
 	if (size > MAX_HEAP_BUCKET_SIZE)
 	{
 		/* Get vm_pages directly from vm subsystem */
 		// TBD
-		return (void*)(free_buffer);
+		return (void*)(ret_va);
 	}
 
 	if (!(*cache_entry))
@@ -100,12 +110,12 @@ void* malloc (UINT32 size, CACHE_PTR *cache_entry)
 	/* Follow the hierarchy to fetch memory */
 
 	/* STEP1: 
-	 * Try from free buffer list.	
+	 * Try from partially free slab list.	
 	 */
-	if ( (*cache_entry)->free_buffer_list) {
+	if ( (*cache_entry)->partially_free_slab_list) {
 		/* Get one free buffer and adjust the list. */
-		free_buffer = (*cache_entry)->free_buffer_list;
-		RemoveFromList(&((*cache_entry)->free_buffer_list->list));
+		ret_va = GetFreeBufferFromSlab((*cache_entry)->partially_free_slab_list); 
+		/* This is 100% success because this list exists only if some buffers are free */
 		goto FREE_BUFFER_FOUND;
 	}
 
@@ -117,23 +127,21 @@ FREE_SLAB_FETCH:
 	
 	if (free_slabs_count)
 	{
-		ALLOCATED_SLAB_TREE_PTR new_node;
-
-		free_slab = (*cache_entry)->free_slab_list;
+		free_slab = (*cache_entry)->completely_free_slab_list;
 		/* Now remove this slab from free slab list and 
-		 * insert it into allocated slab tree. 
+		 * insert it into in use slab tree and partially free slab list. 
 		 */
-		RemoveFromList(&((*cache_entry)->free_slab_list->list));
-		new_node = CreateNodeAllocatedSlabTree(free_slab);
-		InsertNodeIntoAvlTree((AVL_TREE_PTR*)&((*cache_entry)->allocated_slab_tree),
-				(AVL_TREE_PTR)(&(new_node->tree)));
-		new_node->reference_count = 1;
+		RemoveFromList(&((*cache_entry)->completely_free_slab_list->completely_free_list));
+		InsertNodeIntoAvlTree((AVL_TREE_PTR*)&((*cache_entry)->in_use_slab_tree_root),
+				(&(free_slab->in_use_tree)));
+		
+		AddToListTail(&((*cache_entry)->partially_free_slab_list->partially_free_list),
+				(&(free_slab->partially_free_list)));
+
+		free_slab->used_buffer_count = 1;
 		
 		/* Now get one buffer from this slab */	
-		free_buffer = free_slab->free_buffer_list;
-		RemoveFromList(&(free_slab->free_buffer_list->list));
-		/* Now put the free buffers in this slab into free buffer list */
-		AddToListTail(&((*cache_entry)->free_buffer_list->list), &(free_slab->free_buffer_list->list));
+		ret_va = GetFreeBufferFromSlab(free_slab);
 		goto FREE_BUFFER_FOUND;
 	}
 
@@ -150,19 +158,8 @@ FREE_SLAB_FETCH:
 	}
 
 FREE_BUFFER_FOUND:
-	return free_buffer;
+	return (void*)ret_va;
 }
-
-static ALLOCATED_SLAB_TREE_PTR CreateNodeAllocatedSlabTree (SLAB_PTR slab_entry)
-{
-	/* The new node occupies memory in vm_page tha's meant for meta data */
-	ALLOCATED_SLAB_TREE_PTR new_node = (ALLOCATED_SLAB_TREE_PTR)(GET_META_DATA(slab_entry));
-	new_node->slab = slab_entry;
-	new_node->reference_count = 0;
-	new_node->count_virtual_pages = slab_entry->count_virtual_pages;
-	return new_node;
-}
-
 
 /*!
 	\brief	 Adds slabs to cache by requesting memory from VM.
@@ -174,7 +171,7 @@ static ALLOCATED_SLAB_TREE_PTR CreateNodeAllocatedSlabTree (SLAB_PTR slab_entry)
 		0	if successfully fetched from VM 
 		-1 	if failure.
 */
-int AddSlabToCache (CACHE_PTR cache_entry)
+static int AddSlabToCache (CACHE_PTR cache_entry)
 {
 	/* Request vm for vm_pages and add it to a slab.
 	 * No of vm_pages is decided based on size of cache.
@@ -183,4 +180,24 @@ int AddSlabToCache (CACHE_PTR cache_entry)
 
 	(cache_entry->free_slabs_count)++;
 	return 0;
+}
+
+
+
+/*!
+	\brief	Gets a free Buffer from the given slab.
+
+	\param
+		free_slab:	Pointer to the slab
+
+	\return	 Virtual address of the free buffer.
+*/
+static VADDR GetFreeBufferFromSlab(SLAB_PTR free_slab)
+{
+	VADDR ret_va;
+	
+	//TBD
+	//100% get a free buffer.
+	//If all buffers utilized, remove from list!
+	return ret_va;
 }

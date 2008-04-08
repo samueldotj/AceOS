69b549980b3bf2f965f582118cc92d020559758b
nmdilipsimha <nmdilipsimha@78f6c469-4e3a-0410-bca2-dfd793a85426>
2008-04-08 18:24:25 +0000

    Modified logic for breaking the for loop in finding clear/set bit in bitarray. Added parantheses around macro arguments.
    git-svn-id: svn://svn.code.sf.net/p/aceos/code@148 78f6c469-4e3a-0410-bca2-dfd793a85426

--- src/include/bits.h
+++ src/include/bits.h
@@ -4,7 +4,7 @@
   \version 	3.0
   \date	
   			Created: 3:29 PM 07-Apr-08
-  			Last modified: 3:29 PM 07-Apr-08
+  			Last modified: Tue Apr 08, 2008  11:52PM
   \brief	Contains bit manipulation macros.
 */
 
@@ -13,22 +13,24 @@
 
 /*gets the bit value 1 or 0 of the specified index in a bit array - bit_index starts from 0*/
 #define BIT_ARRAY_GET_BIT(bit_array, bit_index) \
-	( (((BYTE *(bit_array))[bit_index / BITS_PER_BYTE]) >> (bit_index % BITS_PER_BYTE) ) & 1 )
+	( (((BYTE *(bit_array))[(bit_index) / BITS_PER_BYTE]) >> ((bit_index) % BITS_PER_BYTE) ) & 1 )
 
 /*sets the bit value to 1 in a bit array for a specified bit_index; bit_index starts from 0*/
 #define BIT_ARRAY_SET_BIT(bit_array, bit_index) \
-	(BYTE * (bit_array))[bit_index / BITS_PER_BYTE] |= (1<< (bit_index % BITS_PER_BYTE) )
+	(BYTE * (bit_array))[(bit_index) / BITS_PER_BYTE] |= (1<< ((bit_index) % BITS_PER_BYTE) )
 
 /*resets the bit value to 0 in a bit array for a specified bit_index; bit_index starts from 0*/
 #define BIT_ARRAY_CLEAR_BIT(bit_array, bit_index) \
-	(BYTE * (bit_array))[bit_index / BITS_PER_BYTE] &= (~(1<< (bit_index % BITS_PER_BYTE) ) )
+	(BYTE * (bit_array))[(bit_index) / BITS_PER_BYTE] &= (~(1<< ((bit_index) % BITS_PER_BYTE) ) )
 
-/*finds the first set bit in a bit array*/
+/* Finds the first set bit in a bit array
+ * If Not found, returns -1
+ */
 #define BIT_ARRAY_FIND_FIRST_SET_BIT(bit_array, length_in_byte, result) 		\
 	{																			\
-		int i, found=0;															\
-		result = 0;																\
-		for(i=0; i<length_in_byte && !found;i++)								\
+		int i, found=FALSE;														\
+		(result) = (length_in_byte);											\
+		for(i=0; i<(length_in_byte) && !found;i++)								\
 		{																		\
 			register BYTE byte = (BYTE * (bit_array))[i];						\
 			if ( byte != 0 )													\
@@ -38,20 +40,27 @@
 				{																\
 					if ( byte  & (1<<j) )										\
 					{															\
-						result = (i * BITS_PER_BYTE) + j;						\
+						(result) = (i * BITS_PER_BYTE) + j;						\
 						found = TRUE;											\
 						break;													\
 					}															\
 				}																\
 			}																	\
 		}																		\
-	}																		
-/*finds the first clear bit in a bit array*/
+		if ((result) == (length_in_byte))										\
+		{																		\
+			(result) = -1;														\
+		}																		\
+	}
+
+/* Finds the first clear bit in a bit array
+ * Returns -1 if not found
+ */
 #define BIT_ARRAY_FIND_FIRST_CLEAR_BIT(bit_array, length_in_byte, result) 		\
 	{																			\
-		int i, found = 0;														\
-		result = 0;																\
-		for(i=0;i<length_in_byte && !found ;i++)								\
+		int i, found = FALSE;													\
+		(result) = (length_in_byte);											\
+		for(i=0;i<(length_in_byte) && !found ;i++)								\
 		{																		\
 			register BYTE byte = (BYTE * (bit_array))[i];						\
 			if ( byte != 0xFF )													\
@@ -61,13 +70,17 @@
 				{																\
 					if ( !(byte  & (1<<j)) )									\
 					{															\
-						result = (i * BITS_PER_BYTE) + j;						\
+						(result) = (i * BITS_PER_BYTE) + j;						\
 						found = TRUE;											\
 						break;													\
 					}															\
 				}																\
 			}																	\
 		}																		\
-	}	
+		if ((result) == (length_in_byte))										\
+		{																		\
+			(result) = -1;														\
+		}																		\
+	}
 	   
 #endif

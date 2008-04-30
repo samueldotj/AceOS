ad87e351591aaa49e5478f4a5c1846cba2e3abf0
samueldotj <samueldotj@78f6c469-4e3a-0410-bca2-dfd793a85426>
2008-04-30 13:05:13 +0000

    Modified the run_test.sh to run the test suites multiple times with different values
    git-svn-id: svn://svn.code.sf.net/p/aceos/code@204 78f6c469-4e3a-0410-bca2-dfd793a85426

--- src/lib/heap/test/run_test.sh
+++ src/lib/heap/test/run_test.sh
@@ -1,1 +1,4 @@
 ./testslab /alloc_count 100 /cache_size 512 /min_slabs 10 /max_slabs 100 $1 
+./testslab /alloc_count 30 /cache_size 100 /min_slabs 50 /max_slabs 60 $1
+./testslab /alloc_count 70 /cache_size 25 /min_slabs 100 /max_slabs 100 $1
+./testslab /alloc_count 100 /cache_size 25 /min_slabs 100 /max_slabs 100 $1

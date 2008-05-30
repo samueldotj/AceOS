54ef2d74d56df879c6152d70d8b79cab65a4568a
nmdilipsimha <nmdilipsimha@78f6c469-4e3a-0410-bca2-dfd793a85426>
2008-05-30 03:00:11 +0000

    added support for pit and pmem
    git-svn-id: svn://svn.code.sf.net/p/aceos/code@231 78f6c469-4e3a-0410-bca2-dfd793a85426

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -4,7 +4,7 @@
   \version 	3.0
   \date	
   			Created: Fri Sep 21, 2007  02:26PM
-  			Last modified: Fri Oct 12, 2007  04:27PM
+  			Last modified: Thu May 29, 2008  10:04AM
   \brief		
 */
 #include <version.h>
@@ -13,11 +13,13 @@
 #include <kernel/debug.h>
 #include <kernel/gdb.h>
 #include <kernel/multiboot.h>
+#include <kernel/mm/pmem.h>
 #include <kernel/time.h>
+#include <kernel/pit.h>
 
 /*! first C function which gets control from assembly
 */
-void cmain(unsigned long magic, multiboot_info_t * mbi)
+void cmain(unsigned long magic, MULTIBOOT_INFO_PTR mbi)
 {
 	SYSTEM_TIME boot_time;
 	int a=0, b=1;
@@ -39,7 +41,14 @@ void cmain(unsigned long magic, multiboot_info_t * mbi)
 	}
 	GetBootTime( &boot_time );
 	kprintf("Boot time : %d-%d-%d %d:%d\n", boot_time.day, boot_time.month, boot_time.year, boot_time.hour, boot_time.minute);
-	
+
+	/* Initialize PIT with the specified frequency */
+	if ( InitPit(TIMER_FREQUENCY) )	
+	{
+		kprintf("PIT Initialization failed");
+		while(1);
+	}
+
 	/*initialize kernel parameters and parse boot parameters*/
 	InitKernelParameters();
 	ParaseBootParameters();
@@ -47,8 +56,8 @@ void cmain(unsigned long magic, multiboot_info_t * mbi)
 	/*start gdb as soon as possible*/
 	if ( sys_gdb_port )
 		InitGdb();
-
+	while(1);
+	//InitPhysicalMemory(mbi); /* Initiate pmem */
 	//for now generate exception
-	a = b/a;
-	
+	//a = b/a;
 }

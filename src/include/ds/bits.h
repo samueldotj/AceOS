83e2d8491fd40f4c1351f150a78320a1ea9cb613
samueldotj <samueldotj@78f6c469-4e3a-0410-bca2-dfd793a85426>
2008-06-16 13:09:36 +0000

    Updated EXTRACT_BITS by removing the constant 32 and adding appropriate value.
    git-svn-id: svn://svn.code.sf.net/p/aceos/code@259 78f6c469-4e3a-0410-bca2-dfd793a85426

--- src/include/ds/bits.h
+++ src/include/ds/bits.h
@@ -17,7 +17,7 @@
  * x and y are measured from lsb and x < y. x=1 means 1st bit.
  */
 #define EXTRACT_BITS(src, x, y) \
-    (((unsigned long int)src<<(32-y))>>(32-y+x-1))
+    (((unsigned long int)src<<((sizeof(unsigned long int)*BITS_PER_BYTE)-y))>>((sizeof(unsigned long int)*BITS_PER_BYTE)-y+x-1))
 
 inline int GetBitFromBitArray(void * bit_array, UINT32 bit_index);
 inline void SetBitInBitArray(void * bit_array, UINT32 bit_index);

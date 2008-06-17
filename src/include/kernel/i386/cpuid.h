ac484725a103f8468af274b0274b2924045e1db2
nmdilipsimha <nmdilipsimha@78f6c469-4e3a-0410-bca2-dfd793a85426>
2008-06-17 09:34:43 +0000

    added some functionality into cpuid
    git-svn-id: svn://svn.code.sf.net/p/aceos/code@265 78f6c469-4e3a-0410-bca2-dfd793a85426

--- src/include/kernel/i386/cpuid.h
+++ src/include/kernel/i386/cpuid.h
@@ -1,10 +1,10 @@
 /*!
-  \file		cpuid.h
+  \file		kernel/i386/cpuid.h
   \author	DilipSimha N M
   \version 	3.0
   \date	
   			Created: Sat Jun 14, 2008  06:18PM
-  			Last modified: Mon Jun 16, 2008  02:40PM
+  			Last modified: Tue Jun 17, 2008  03:04PM
   \brief	
 */
 
@@ -13,10 +13,76 @@
 #define _CPUID_H_
 #include <ace.h>
 
+typedef union feature_flags_edx
+{
+	UINT32 feature_flags; /* Placeholder to split the integer into bits */
+	struct
+	{
+		UINT32 fpu_on_chip: 1; //0
+		UINT32 virtual_mode_extn: 1; //1
+		UINT32 debugging_exten: 1; //2
+		UINT32 page_size_exten: 1; //3
+		UINT32 time_stamp_counter: 1; //4
+		UINT32 model_specific_registers: 1; //5
+		UINT32 physical_address_exten: 1; //6
+		UINT32 machine_check_exten: 1; //7
+		UINT32 cmpxchg8: 1; //8
+		UINT32 apic : 1; //9
+		UINT32 reserved1 : 1; //10
+		UINT32 fast_system_call : 1; //11
+		UINT32 memory_type_range_registers : 1; //12
+		UINT32 page_global_enable : 1; //13
+		UINT32 machine_check_architecture : 1; //14
+		UINT32 conditional_move_instr : 1; //15
+		UINT32 page_attribute_table : 1; //16
+		UINT32 page_size_extn_32bit : 1; //17
+		UINT32 processor_serial_number : 1; //18
+		UINT32 clflush_instr : 1; //19
+		UINT32 reserved2 : 1; //20
+		UINT32 debug_store : 1; //21
+		UINT32 acpi : 1; //22
+		UINT32 mmx : 1; //23
+		UINT32 fast_floating_point : 1; //24
+		UINT32 sse : 1; //streaming SIMD extension //25
+		UINT32 sse2 : 1; //26
+		UINT32 self_snoop : 1; //27
+		UINT32 multi_threading : 1; //28
+		UINT32 thermal_monitor : 1; //29
+		UINT32 ia64 : 1; //30
+		UINT32 pending_break_enable : 1; //31
+	};
+}FEATURE_FLAGS_EDX, * FEATURE_FLAGS_EDX_PTR;
+
+
+typedef union feature_flags_ecx
+{
+	UINT32 feature_flags;  /* Placeholder to split the integer into bits */
+	struct
+	{
+		UINT32  sse3:1 ; //0
+		UINT32  reserved1:1 ; //1
+		UINT32  debug_store_64bit:1 ; //2
+		UINT32  monitor_mwait:1 ; //3
+		UINT32  ds_cpl:1 ; //CPL qualified debug store //4
+		UINT32  virtual_machine_extn:1 ; //5
+		UINT32  safer_mode_extn:1 ; //6
+		UINT32  est:1 ; //enhanced Intel SpeedStep technology //7
+		UINT32  thermal_monitor2:1 ; //8
+		UINT32  ssse3:1 ; //Supplemental Streaming SIMD Extensions 3 //9
+		UINT32  context_id:1 ; //10
+		UINT32  reserved2:2 ; //11,12
+		UINT32  cmpxchg_16bytes:1 ; //13
+		UINT32  send_task_priority_msg:1 ; //14
+		UINT32  perfromance_capability_msr:1 ; //15
+		UINT32  reserved3:2 ; //16,17
+		UINT32  direct_cache_access:1 ; //18
+		UINT32  reserved4:13 ; //19-31
+	};
+}FEATURE_FLAGS_ECX, * FEATURE_FLAGS_ECX_PTR;
+
 typedef struct cpuid_info
 {
     UINT32 eax_max;
-    UINT32 apic_id;
     char vendor_id[20];
     UINT32 stepping_id;
     UINT32 model_number;
@@ -27,7 +93,9 @@ typedef struct cpuid_info
     UINT32 brand_id;
     UINT32 cache_line_size;
     UINT32 count_logical_processors;
-    UINT32 bit_array;
+    UINT32 apic_id;
+	FEATURE_FLAGS_EDX feature_flags_edx;
+	FEATURE_FLAGS_ECX feature_flags_ecx;
     UINT32 serial_number_low;
     UINT32 serial_number_high;
     UINT32 processor_label;
--- src/kernel/i386/cpuid.c
+++ src/kernel/i386/cpuid.c
@@ -4,17 +4,124 @@
   \version 	3.0
   \date	
   			Created: Sun Jun 15, 2008  12:23PM
-  			Last modified: Tue Jun 17, 2008  09:03AM
+  			Last modified: Tue Jun 17, 2008  03:05PM
   \brief	Provides support for cpu identification.
 */
 
 #include <ace.h>
 #include <string.h>
 #include <kernel/i386/cpuid.h>
+#include <ds/bits.h>
 
 static CPUID_INFO cpuid_info;
 
+#define EXEC_CPUID_INSTRUCTION	__asm__ volatile("mov %4, %%eax; CPUID; mov %%eax, %0; mov %%ebx, %1; mov %%ecx, %2; mov %%edx, %3" \
+					:"=m"(reg_eax), "=m"(reg_ebx), "=m"(reg_ecx), "=m"(reg_edx) \
+					:"m"(input) \
+					);
 
+void GetAttributesFromEax0(void)
+{
+	UINT32 reg_eax, reg_ebx, reg_ecx, reg_edx, input;
+
+	/* Load EAX with 0 and call CPUID instruction */
+	input=0;
+	//EXEC_CPUID_INSTRUCTION();
+	__asm__ volatile("mov %4, %%eax; CPUID; mov %%eax, %0; mov %%ebx, %1; mov %%ecx, %2; mov %%edx, %3" 
+					:"=m"(reg_eax), "=m"(reg_ebx), "=m"(reg_ecx), "=m"(reg_edx)
+                    :"m"(input)
+                    );
+	/* Output in EAX */
+	cpuid_info.eax_max = reg_eax;
+	
+	/* Output in EBX */
+	cpuid_info.vendor_id[0] = EXTRACT_BITS(reg_ebx, 0, 7);
+	cpuid_info.vendor_id[1] = EXTRACT_BITS(reg_ebx, 8, 15);
+	cpuid_info.vendor_id[2] = EXTRACT_BITS(reg_ebx, 16, 23);
+	cpuid_info.vendor_id[3] = EXTRACT_BITS(reg_ebx, 24, 31);
+
+	/* Output in ECX */
+	cpuid_info.vendor_id[4] = EXTRACT_BITS(reg_ecx, 0, 7);
+	cpuid_info.vendor_id[5] = EXTRACT_BITS(reg_ecx, 8, 15);
+	cpuid_info.vendor_id[6] = EXTRACT_BITS(reg_ecx, 16, 23);
+	cpuid_info.vendor_id[7] = EXTRACT_BITS(reg_ecx, 24, 31);
+	
+	/* Output in EDX */
+	cpuid_info.vendor_id[8] = EXTRACT_BITS(reg_edx, 0, 7);
+	cpuid_info.vendor_id[9] = EXTRACT_BITS(reg_edx, 8, 15);
+	cpuid_info.vendor_id[10] = EXTRACT_BITS(reg_edx, 16, 23);
+	cpuid_info.vendor_id[11] = EXTRACT_BITS(reg_edx, 24, 31);
+	cpuid_info.vendor_id[12] = '\0';
+	return;
+}
+
+
+void GetAttributesFromEax1(void)
+{
+	UINT32 reg_eax, reg_ebx, reg_ecx, reg_edx, input;
+
+	/* Load EAX with 1 and call CPUID instruction */
+	input=1;
+	//EXEC_CPUID_INSTRUCTION();
+	__asm__ volatile("mov %4, %%eax; CPUID; mov %%eax, %0; mov %%ebx, %1; mov %%ecx, %2; mov %%edx, %3" 
+					:"=m"(reg_eax), "=m"(reg_ebx), "=m"(reg_ecx), "=m"(reg_edx)
+                    :"m"(input)
+                    );
+	/* Output in EAX */
+	cpuid_info.stepping_id = EXTRACT_BITS(reg_eax, 0, 3);
+	cpuid_info.model_number = EXTRACT_BITS(reg_eax, 4, 7);
+	cpuid_info.family_code = EXTRACT_BITS(reg_eax, 8, 11);
+	cpuid_info.processor_type = EXTRACT_BITS(reg_eax, 12, 13);
+	cpuid_info.extended_model = EXTRACT_BITS(reg_eax, 16, 19);
+	cpuid_info.extended_family = EXTRACT_BITS(reg_eax, 20, 27);
+	
+	/* Output in EBX */
+	cpuid_info.brand_id = EXTRACT_BITS(reg_ebx, 0, 7);
+	cpuid_info.cache_line_size = EXTRACT_BITS(reg_ebx, 8, 15); //this is same as chunks
+	cpuid_info.count_logical_processors = EXTRACT_BITS(reg_ebx, 16, 23);
+	cpuid_info.apic_id = EXTRACT_BITS(reg_ebx, 24, 31);
+
+	/* Output in ECX */
+	cpuid_info.feature_flags_ecx.feature_flags = reg_ecx;
+	/* Output in EDX */
+	cpuid_info.feature_flags_edx.feature_flags = reg_edx;
+}
+
+void GetAttributesFromEax2(void)
+{
+	UINT32 reg_eax, reg_ebx, reg_ecx, reg_edx, input;
+
+	/* Load EAX with 2 and call CPUID instruction */
+	input=2;
+	//EXEC_CPUID_INSTRUCTION();
+	__asm__ volatile("mov %4, %%eax; CPUID; mov %%eax, %0; mov %%ebx, %1; mov %%ecx, %2; mov %%edx, %3" 
+					:"=m"(reg_eax), "=m"(reg_ebx), "=m"(reg_ecx), "=m"(reg_edx)
+                    :"m"(input)
+                    );
+	/* Output in EAX */
+	
+	/* Output in EBX */
+	/* Output in ECX */
+	/* Output in EDX */
+}
+
+void GetAttributesFromEax3(void)
+{
+	UINT32 reg_eax, reg_ebx, reg_ecx, reg_edx, input;
+
+	/* Load EAX with 3 and call CPUID instruction */
+	input=3;
+	//EXEC_CPUID_INSTRUCTION();
+	__asm__ volatile("mov %4, %%eax; CPUID; mov %%eax, %0; mov %%ebx, %1; mov %%ecx, %2; mov %%edx, %3" 
+					:"=m"(reg_eax), "=m"(reg_ebx), "=m"(reg_ecx), "=m"(reg_edx)
+                    :"m"(input)
+                    );
+	/* Output in EAX */
+	
+	/* Output in EBX */
+	/* Output in ECX */
+	/* Output in EDX */
+}
 
 /*!
 	\brief	Loads the cpuid_info structure with all available cpuid information. 
@@ -27,6 +134,18 @@ void LoadDataStructureFromCpuID()
 {
 	/* call CPUID with all possible values and store all the required information */
 	/* After this call, we can just read value from data structures without execting CPUID instruction every time */
+
+	/* EAX=0 */
+	GetAttributesFromEax0();
+
+	/* EAX=1 */
+	GetAttributesFromEax1();
+
+	/* EAX=2 */
+	GetAttributesFromEax2();
+
+	/* EAX=3 */
+	GetAttributesFromEax3();
 }
 
 
@@ -44,7 +163,6 @@ void * GetFromCpuId(int command)
 	switch(command)
 	{
 		case EAX_MAX					: return &(cpuid_info.eax_max);
-		case APIC_ID					: return &(cpuid_info.apic_id);
 		case VENDOR_ID					: return &(cpuid_info.vendor_id);
 		case STEPPING_ID				: return &(cpuid_info.stepping_id);
 		case MODEL_NUMBER				: return &(cpuid_info.model_number);
@@ -55,7 +173,8 @@ void * GetFromCpuId(int command)
 		case BRAND_ID					: return &(cpuid_info.brand_id);
 		case CACHE_LINE_SIZE			: return &(cpuid_info.cache_line_size);
 		case COUNT_LOGICAL_PROCESSORS	: return &(cpuid_info.count_logical_processors);
-		case BIT_ARRAY					: return &(cpuid_info.bit_array);
+		case APIC_ID					: return &(cpuid_info.apic_id);
+		//TODO.. feature flags for eax=1
 		case SERIAL_NUMBER_LOW			: return &(cpuid_info.serial_number_low);
 		case SERIAL_NUMBER_HIGH			: return &(cpuid_info.serial_number_high);
 		case PROCESSOR_LABEL			: return &(cpuid_info.processor_label);
@@ -85,3 +204,8 @@ int DetectCpuId(void)
 	return 0;
 }
 
+
+
+void GetFeatureFlagsEax1(UINT32 reg_ecx, UINT32 reg_edx)
+{
+}
